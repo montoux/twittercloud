8cb41485d7df79272425a6c61555a55efb750a99
Stephen Nelson <stephen@montoux.com>
2017-05-24 17:20:46 +1200

    Cleaned up and ready for workshop

--- src/twittercloud/core.clj
+++ src/twittercloud/core.clj
@@ -1,18 +1,17 @@
 (ns twittercloud.core
   (:gen-class)
   (:require [clojure.data.json :as json]
-            [clojure.java.io :as io])
-  (:import (java.nio.file Paths Path Files)
+            [clojure.edn :as edn]
+            [clojure.java.io :as io]
+            [clojure.string :as str]
+            [clojure.test :refer :all])
+  (:import (com.twitter.hbc.httpclient.auth OAuth1)
+           (java.io InputStream BufferedReader)
            (java.nio.charset StandardCharsets)
-           (java.io PushbackReader)
-           (java.net URLEncoder URLDecoder)
-           (java.util Base64)
-           (com.google.common.io BaseEncoding)
-           (com.twitter.hbc.httpclient.auth OAuth1)
-           (org.apache.http.client.methods HttpGet HttpPost)
+           (java.nio.file Files Path Paths)
+           (org.apache.http HttpRequest HttpResponse)
            (org.apache.http.impl.client DefaultHttpClient)
-           (org.apache.http HttpResponse)
-           (org.apache.http.client HttpClient)))
+           (org.apache.http.client.methods HttpGet HttpPost)))
 
 
 (extend-type Path
@@ -21,51 +20,56 @@
     (Files/newBufferedReader p StandardCharsets/UTF_8)))
 
 
+(defn resolve-path [base & more]
+  (Paths/get base (into-array String more)))
+
+
 (defn get-auth []
-  (let [path (Paths/get (System/getenv "HOME") (into-array String [".twitter" "auth.clj"]))
-        auth (PushbackReader. (io/reader path))]
-    (read auth)))
+  (-> (System/getenv "HOME")
+      (resolve-path ".twitter" "auth.clj")
+      (slurp)
+      (edn/read-string)))
 
 
-(defn sample-tweets [{:keys [consumer-key consumer-secret access-token access-token-secret]}]
-  (let [client   (DefaultHttpClient.)
-        oauth    (OAuth1. consumer-key consumer-secret access-token access-token-secret)
-        req      (HttpGet. "https://stream.twitter.com/1.1/statuses/sample.json")
-        _        (.signRequest oauth req "")
-        response ^HttpResponse (.execute client req)]
-    (when (not= 200 (-> response (.getStatusLine) (.getStatusCode)))
-      (throw (IllegalStateException. (.getReasonPhrase (.getStatusLine response)))))
+(defn sign
+  "Given a twitter authentication map and a HttpRequest, signs and returns the request."
+  {:tag HttpRequest}
+  [^HttpRequest request auth]
+  (let [{:keys [consumer-key consumer-secret access-token access-token-secret]} auth
+        oauth (OAuth1. consumer-key consumer-secret access-token access-token-secret)]
+    (.signRequest oauth request "")
+    request))
+
+
+(defn execute
+  "Performs an HttpRequest. Returns an InputStream on 200 response, otherwise throws."
+  [^HttpRequest request]
+  (let [client (DefaultHttpClient.)
+        response ^HttpResponse (.execute client request)
+        status (.getStatusLine response)]
+    (when (not= 200 (.getStatusCode status))
+      (throw (IllegalStateException. (.getReasonPhrase status))))
     (.getContent (.getEntity response))))
 
 
-(defn filter-tweets [{:keys [consumer-key consumer-secret access-token access-token-secret]} filter]
-  (let [client   (DefaultHttpClient.)
-        oauth    (OAuth1. consumer-key consumer-secret access-token access-token-secret)
-        req      (HttpPost. (str "https://stream.twitter.com/1.1/statuses/filter.json?track=" filter))
-        _        (.signRequest oauth req "")
-        response ^HttpResponse (.execute client req)]
-    (when (not= 200 (-> response (.getStatusLine) (.getStatusCode)))
-      (throw (IllegalStateException. (.getReasonPhrase (.getStatusLine response)))))
-    (.getContent (.getEntity response))
-    ))
+(defn sample-tweets
+  "Read tweets from the twitter sample stream, returns an InputStream that must be consumed."
+  {:tag InputStream}
+  [auth]
+  (-> (HttpGet. "https://stream.twitter.com/1.1/statuses/sample.json")
+      (sign auth)
+      (execute)))
+
+
+(defn filter-tweets
+  "Read tweets from the twitter sample stream, but use a filter to match tweets that include
+  our search term."
+  {:tag InputStream}
+  [auth filter]
+  (-> (HttpPost. (str "https://stream.twitter.com/1.1/statuses/filter.json?track=" filter))
+      (sign auth)
+      (execute)))
 
 
 (defn -main [& args]
   (println "Hello World!"))
-
-
-#_(with-open [is (sample-tweets (get-auth))
-              ir (InputStreamReader. is)
-              br (BufferedReader. ir)]
-    (transduce
-      (comp (filter #(re-find #"\"lang\":\"en\"" %))
-            (keep (comp second #(re-find #"text\":\"([^\"]+)\"" %)))
-            (mapcat #(str/split % #"\s+"))
-            (filter #(re-find #"^#" %))
-            (take 100))
-      (fn
-        ([] (transient {}))
-        ([counts x] (assoc! counts x (inc (get counts x 0))))
-        ([counts] (persistent! counts)))
-      (repeatedly #(.readLine br))
-      ))
